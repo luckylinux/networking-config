c57cb441cc34b912f03dfb57de246850b0772ece
luckylinux <luckylinux>
2024-06-14 09:22:23 +0200

    Ignore config.sh Configuration File which is HOST-specific.

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Setup snid networking ?
-# Probably not needed for Desktops
-SETUP_SNID_NETWORKING="no"
-
-# Setup containers networking
-# Probably not needed for Desktops
-SETUP_CONTAINERS_NETWORKING="yes"
