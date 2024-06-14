97ae3967258827db968f4ff5e3e37dd72116886d
luckylinux <luckylinux>
2024-06-14 09:41:43 +0200

    Added parametrized Configuration and Echo Details when starting up the snid Server.

--- etc/networking-snid/servers.local.d/example.sh
+++ etc/networking-snid/servers.local.d/example.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Wait a bit until System is properly booted up
+sleep 15
+
+# Configuration
+IPV4_ADDRESS="192.168.8.25"
+IPV4_PORT="443"
+IPV4_TYPE="tcp"
+NAT46_PREFIX="64:ff9b:1::9999:0:0"
+IPV6_BACKEND_CIDR="2001:db8:0000:0001:0000:0000:0001:0000/112"
+
+# Echo
+echo "Starting snid Server: Listening on ${IPV4_TYPE}:${IPV4_ADDRESS}:${IPV4_PORT}. Using NAT46 Prefix ${NAT46_PREFIX} with CIDR Backend ${IPV6_BACKEND_CIDR}"
+
 # Run the Server
-/opt/snid/snid -listen tcp:192.168.8.25:443 -mode nat46 -nat46-prefix 64:ff9b:1::9999:0:0 -backend-cidr 2001:db8:0000:0001:0000:0000:0001:0000/112
+/opt/snid/snid -listen "${IPV4_TYPE}":"${IPV4_ADDRESS}":"${IPV4_PORT}" -mode nat46 -nat46-prefix "${NAT46_PREFIX}" -backend-cidr "${IPV6_BACKEND_CIDR}"
