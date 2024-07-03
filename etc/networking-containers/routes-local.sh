136daa2f877ac749e72dd89cb5a41c41841d7517
luckylinux <luckylinux>
2024-07-03 19:07:45 +0200

    Fix addresses -> routes in Path.

--- etc/networking-containers/routes-local.sh
+++ etc/networking-containers/routes-local.sh
@@ -4,7 +4,7 @@
 action=${1-"start"}
 
 # Get list of Configuration Scripts
-mapfile -t files < <( find "/etc/networking-containers/addresses.local.d/" -iname "*.sh" )
+mapfile -t files < <( find "/etc/networking-containers/routes.local.d/" -iname "*.sh" )
 
 # Process each File
 for file in "${files[@]}"
